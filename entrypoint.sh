ff2a62ad81e0ed9c273829f76da5041900b1e7ac
RRyanward <jake1161@ryandward.com>
2024-09-27 00:34:00 -0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,14 @@ if [ -e "stop-install-loop.sh" ]; then
     echo 'File already exists' >&2
 else
 curl -sL https://raw.githubusercontent.com/arkmanager/ark-server-tools/master/netinstall.sh | bash -s container --me --perform-user-install --yes-i-really-want-to-perform-a-user-install
+cp /home/container/bin/arkmanager /home/container/arkmanger
 fi
 echo "Adding install loop file" | tee stop-install-loop.sh
 echo "Ark Server Tools Ready"
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ~/bin/arkmanager ${INSTALL_ARK} ${RUN_ARK} ${UPDATE_ARK} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=`eval echo $(echo ./arkmanger ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
